0194348a652733a583c3d8338a421050165c2389
awalex <santini.alex@gmail.com>
2014-11-20 15:18:04 +0100

    once again, url fix

--- core/urls.py
+++ core/urls.py
@@ -4,29 +4,14 @@ from tastypie.api import Api
 from apps.filehandler.api import OddlyFileHandling, TaskManagerEntry
 
 from core.api.endpoints.item_handling import ItemHandlingResource
-'''
-Tastypie ApiCdn
-__version__ : v1
-
-Register models :
-    BetaccountEntry
-    BookResource
-'''
-
-v1_api = Api(api_name = 'v1')
-v1_api.register(OddlyFileHandling())
-v1_api.register(TaskManagerEntry())
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'core.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(v1_api.urls)),
+    url(r'^api/v1/', include(v1_api.urls)),
     url(r'^', include(ItemHandlingResource.urls())),
     url(r'^thumb/[0-9a-fA-F]{24}$', 'apps.filehandler.views.get_thumb', name='get_thumb')
     )
