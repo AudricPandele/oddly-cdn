89b792e0099a60128917ca5eb880186bf406d15f
awalex <santini.alex@gmail.com>
2014-11-20 15:20:00 +0100

    once more again, url fix

--- core/urls.py
+++ core/urls.py
@@ -11,7 +11,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/v1/', include(v1_api.urls)),
-    url(r'^', include(ItemHandlingResource.urls())),
+    url(r'^api/v1/', include(ItemHandlingResource.urls())),
     url(r'^thumb/[0-9a-fA-F]{24}$', 'apps.filehandler.views.get_thumb', name='get_thumb')
     )
