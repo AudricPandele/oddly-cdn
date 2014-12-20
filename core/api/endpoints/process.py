7c204369048e348dffccb54595fc122f8e71d865
awalex <santini.alex@gmail.com>
2014-12-20 11:24:48 +0100

    Updating endpoint to get process status

---
+++ core/api/endpoints/process.py
@@ -0,0 +1,58 @@
+import base64
+import json
+import requests
+import os
+
+from django.views.decorators.csrf import csrf_exempt
+from django.conf.urls import patterns, url
+from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+
+from restless.dj import DjangoResource
+from restless.preparers import FieldsPreparer
+from restless.resources import skip_prepare
+from restless.exceptions import Unauthorized
+
+# Import models
+from apps.filehandler.models import FileHandler
+from apps.filehandler import fileprocessing
+
+from apps.taskmanager.models import TaskManager
+
+from apps.filehandler.tasks import run_parser
+
+from pgmagick import Image
+
+#---------------------------------------------------------------------------
+class ProcessResource(DjangoResource):
+    
+    #---------------------------------------------------------------------------
+    def __init__(self, *args, **kwargs):
+        super(ProcessResource, self).__init__(*args, **kwargs)
+
+        self.http_methods.update({
+            'processdetail': {
+                'GET': 'processdetail',
+            },
+        })
+
+    #---------------------------------------------------------------------------
+    def is_authenticated(self):
+        return True
+            
+    #---------------------------------------------------------------------------
+    @skip_prepare
+    def processdetail(self, mongoid):
+        status = TaskManager.objects.get(book_id=mongoid)
+        return {
+            "status":status.process_status
+            }
+        
+    #---------------------------------------------------------------------------
+    @classmethod
+    def urls(cls, name_prefix=None):
+        urlpatterns = super(ProcessResource, cls).urls(name_prefix=name_prefix)
+        return urlpatterns + patterns('',
+            url(r'^status/([0-9a-fA-F]{24})$', csrf_exempt(cls.as_view('processdetail')), name=cls.build_url_name('processdetail', name_prefix)),
+        )
--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ from apps.filehandler.api import OddlyFileHandling, TaskManagerEntry
 from core.api.endpoints.item import ItemHandlingResource
 from core.api.endpoints.artist import ArtistHandlingResource
 
+from core.api.endpoints.process import ProcessResource
+
 urlpatterns = patterns('',
     url(r'^teapot/', include(ItemHandlingResource.urls())),
     url(r'^teapot/', include(ArtistHandlingResource.urls())),
@@ -15,4 +17,5 @@ urlpatterns = patterns('',
                        
     url(r'^item/([0-9a-fA-F]{24})/([A-Z]{2})/([0-9])$', 'apps.filehandler.views.item_file', name='item_file'),    
     url(r'^item/cover/([0-9a-fA-F]{24})/([a-z]{,6})$', 'apps.filehandler.views.item_cover', name='item_cover'),    
+    url(r'^', include(ProcessResource.urls())),    
     )
