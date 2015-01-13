84cf6c9799819014cd3ec06e2ec0b985fa809c3a
Ugo Stephant <contact@dackmin.com>
2015-01-13 23:02:05 +0100

    create uploadImage & convertImage methods

---
+++ utils/tools.py
@@ -0,0 +1,21 @@
+# Python shit
+import os
+
+# Django shit
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+
+# Image shit
+from pgmagick import Image
+
+
+def uploadImage(file = None, relativePath, absolutePath, extensionPath):
+    if file:
+        default_storage.save(relativePath, ContentFile(file.read()))
+        convertImage(oldPath = absolutePath, newPath = extensionPath)
+
+
+def convertImage(oldPath, newPath):
+    _img = Image(oldPath)
+    _img.write(newPath)
+    os.remove(oldPath)
