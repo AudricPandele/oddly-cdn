7f1a91a1627a505662fac051c196c35b8db21412
Ugo Stephant <contact@dackmin.com>
2015-01-13 23:02:54 +0100

    lil refacto deleting old code & adding new tools

--- core/api/endpoints/artist.py
+++ core/api/endpoints/artist.py
@@ -1,118 +1,72 @@
+# Python shit
 import base64
 import json
 import requests
 import os
 
+# Django shit
 from django.views.decorators.csrf import csrf_exempt
 from django.conf.urls import patterns, url
 from django.conf import settings
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 
+# Restless shit
 from restless.dj import DjangoResource
 from restless.preparers import FieldsPreparer
 from restless.resources import skip_prepare
 from restless.exceptions import Unauthorized
 
-# Import models
+# PDF processing
 from apps.filehandler.models import FileHandler
 from apps.filehandler import fileprocessing
-
 from apps.taskmanager.models import TaskManager
-
 from apps.filehandler.tasks import run_parser
 
-from pgmagick import Image
+# Image processing
+from utils.tools import uploadImage
 
-#---------------------------------------------------------------------------
+
 class ArtistHandlingResource(DjangoResource):
-    
-    #---------------------------------------------------------------------------
+
+
     def __init__(self, *args, **kwargs):
         super(ArtistHandlingResource, self).__init__(*args, **kwargs)
 
         self.http_methods.update({
-            'thumbupload': {
-                'POST': 'thumbupload',
-            },
-            'coverupload': {
-                'POST': 'coverupload',
-            }
-        })
+            'thumbupload': { 'POST': 'thumbupload' },
+            'coverupload': { 'POST': 'coverupload' }
+            })
 
-    #---------------------------------------------------------------------------
+
     def is_authenticated(self):
         return True
-            
-    #---------------------------------------------------------------------------
+
+
     @skip_prepare
     def coverupload(self):
-        mongoid = None
-        file_cover = None
+        uploadImage(
+            file = self.data.get("file"),
+            relativePath = str("artist/covers/%s" % (self.data.get("_id"))),
+            absolutePath = "%sartist/covers/%s" %  (settings.MEDIA_ROOT, self.data.get("_id"))),
+            extensionPath = "%sartist/covers/%s.jpg" %  (settings.MEDIA_ROOT, self.data.get("_id")))
+        )
 
-        if self.data.get('file'):
-            # Getting datas 
-            file_cover = self.data.get('file')
-            mongoid = self.data.get('_id')
-            
-        if file_cover is not None:
-            # Actually upload then converting the cover
-            cover_upload_path = "artist/covers/%s" % (mongoid)
-            cover_save = default_storage.save(cover_upload_path, ContentFile(file_cover.read()))
-            convert = self.convert_cover_to_jpeg(current_cover_path = "%sartist/covers/%s" % (settings.MEDIA_ROOT, mongoid),type_image = "covers", mongoid = mongoid)
-        
-        
-    #---------------------------------------------------------------------------
+
     @skip_prepare
     def thumbupload(self):
-        mongoid = None
-        file_cover = None
+        uploadImage(
+            file = self.data.get("file"),
+            relativePath = str("artist/thumbs/%s" % (self.data.get("_id"))),
+            absolutePath = "%sartist/thumbs/%s" %  (settings.MEDIA_ROOT, self.data.get("_id"))),
+            extensionPath = "%sartist/thumbs/%s.jpg" %  (settings.MEDIA_ROOT, self.data.get("_id")))
+        )
 
-        if self.data.get('file'):
-            # Getting datas 
-            file_cover = self.data.get('file')
-            mongoid = self.data.get('_id')
-            
-        if file_cover is not None:
-            # Actually upload then converting the cover
-            cover_upload_path = "artist/thumbs/%s" % (mongoid)
-            cover_save = default_storage.save(cover_upload_path, ContentFile(file_cover.read()))
-            convert = self.convert_cover_to_jpeg(current_cover_path = "%sartist/thumbs/%s" % (settings.MEDIA_ROOT, mongoid), type_image = "thumbs", mongoid = mongoid)
 
-    #---------------------------------------------------------------------------
-    def convert_cover_to_jpeg(self, current_cover_path, mongoid, type_image):
-        """
-        Cover are not always jpeg, so we should convert the uploaded file to jpeg, uh
-        """
-        img = Image(str(current_cover_path))
-        jpegwritepath = str("%sartist/%s/%s.jpg" % (settings.MEDIA_ROOT, type_image, mongoid))
-        img.write(jpegwritepath)
-        remove_tmp_file = os.remove("%sartist/%s/%s" % (settings.MEDIA_ROOT, type_image, mongoid))
-
-    #---------------------------------------------------------------------------
-    def deserialize(self, method, endpoint, body):
-        format = None
-        """
-        Changes request stat in to python objects
-        """
-        if not format:
-            format = self.request.META.get('CONTENT_TYPE', 'application/json')
-
-        if format == 'application/x-www-form-urlencoded':
-            return self.request.POST
-
-        if format.startswith('multipart'):
-            multipart_data = self.request.POST.copy()
-            multipart_data.update(self.request.FILES)
-            return multipart_data
-
-        return super(ArtistHandlingResource, self).deserialize(method, endpoint, format)
-
-    #---------------------------------------------------------------------------
     @classmethod
     def urls(cls, name_prefix=None):
         urlpatterns = super(ArtistHandlingResource, cls).urls(name_prefix=name_prefix)
         return urlpatterns + patterns('',
-                                      url(r'^artist/thumb$', csrf_exempt(cls.as_view('thumbupload')), name=cls.build_url_name('thumbupload', name_prefix)),
+            url(r'^artist/thumb$', csrf_exempt(cls.as_view('thumbupload')), name=cls.build_url_name('thumbupload', name_prefix)),
             url(r'^artist/cover$', csrf_exempt(cls.as_view('coverupload')), name=cls.build_url_name('coverupload', name_prefix)),
-        )
+            )
--- core/api/endpoints/item.py
+++ core/api/endpoints/item.py
@@ -1,85 +1,69 @@
+# Python shit
 import base64
 import json
 import requests
 import os
 
+# Django shit
 from django.views.decorators.csrf import csrf_exempt
 from django.conf.urls import patterns, url
 from django.conf import settings
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 
+# RESTless shit
 from restless.dj import DjangoResource
 from restless.preparers import FieldsPreparer
 from restless.resources import skip_prepare
 from restless.exceptions import Unauthorized
 
-# Import models
+# Images processing
+from utils.tools import uploadImage
+
+# PDF processing
 from apps.filehandler.models import FileHandler
 from apps.filehandler import fileprocessing
-
 from apps.taskmanager.models import TaskManager
-
 from apps.filehandler.tasks import run_parser
 
-from pgmagick import Image
 
-#---------------------------------------------------------------------------
 class ItemHandlingResource(DjangoResource):
 
-    #---------------------------------------------------------------------------
+
     def __init__(self, *args, **kwargs):
         super(ItemHandlingResource, self).__init__(*args, **kwargs)
 
         self.http_methods.update({
-            'fileupload': {
-                'POST': 'fileupload',
-            },
-            'coverupload': {
-                'POST': 'coverupload',
-            },
-            'backgroundupload': {
-                'POST': 'backgroundupload',
-            }
-        })
+            'fileupload': { 'POST': 'fileupload' },
+            'coverupload': { 'POST': 'coverupload' },
+            'backgroundupload': { 'POST': 'backgroundupload' }
+            })
 
-    #---------------------------------------------------------------------------
+
     def is_authenticated(self):
         return True
 
-    #---------------------------------------------------------------------------
+
     @skip_prepare
     def coverupload(self):
-        mongoid = None
-        file_cover = None
+        uploadImage(
+            file = self.data.get("file"),
+            relativePath = str("items/covers/%s" % (self.data.get("_id"))),
+            absolutePath = str("%sitems/covers/%s" % (settings.MEDIA_ROOT, self.data.get("_id"))),
+            extensionPath = str("%sitems/covers/%s.jpg" % (settings.MEDIA_ROOT, self.data.get("_id"))),
+            )
 
-        if self.data.get('file'):
-            # Getting datas
-            file_cover = self.data.get('file')
-            mongoid = self.data.get('_id')
 
-        if file_cover is not None:
-            # Actually upload then converting the cover
-            cover_upload_path = "items/covers/%s" % (mongoid)
-            cover_save = default_storage.save(cover_upload_path, ContentFile(file_cover.read()))
-            convert = self.convert_cover_to_jpeg(current_cover_path = "%sitems/covers/%s" % (settings.MEDIA_ROOT, mongoid), mongoid = mongoid)
-
-    #---------------------------------------------------------------------------
     @skip_prepare
     def backgroundupload(self):
-        mongoid = None
-        file_background = None
+        uploadImage(
+            file = self.data.get("file"),
+            relativePath = str("items/backgrounds/%s" % (self.data.get("_id"))),
+            absolutePath = str("%sitems/backgrounds/%s" % (settings.MEDIA_ROOT, self.data.get("_id"))),
+            extentionPath = str("%sitems/backgrounds/%s.jpg" % (settings.MEDIA_ROOT, self.data.get("_id"))),
+            )
 
-        if self.data.get('file'):
-            file_background = self.data.get('file')
-            mongoid = self.data.get('_id')
 
-        if file_background is not None:
-            bg_upload_path = "items/backgrounds/%s" % (mongoid)
-            bg_save = default_storage.save(bg_upload_path, ContentFile(file_background.read()))
-            convert = self.convert_bg_to_jpeg(current_bg_path = "%sitems/backgrounds/%s" % (settings.MEDIA_ROOT, mongoid), mongoid = mongoid)
-
-    #---------------------------------------------------------------------------
     @skip_prepare
     def fileupload(self):
 
@@ -100,42 +84,7 @@ class ItemHandlingResource(DjangoResource):
             uploaded_file = "%s%s" % (settings.MEDIA_ROOT, upload_path)
             run_parser.delay(uploaded_file=uploaded_file, mongo_id=mongoid)
 
-    #---------------------------------------------------------------------------
-    def convert_cover_to_jpeg(self, current_cover_path, mongoid):
-        """
-        Cover are not always jpeg, so we should convert the uploaded file to jpeg, uh
-        """
-        img = Image(str(current_cover_path))
-        jpegwritepath = str("%sitems/covers/%s.jpg" % (settings.MEDIA_ROOT, mongoid))
-        img.write(jpegwritepath)
-        remove_tmp_file = os.remove("%sitems/covers/%s" % (settings.MEDIA_ROOT, mongoid))
 
-    def convert_bg_to_jpeg(self, current_bg_path, mongoid):
-        img = Image(str(current_bg_path))
-        jpegwritepath = str("%sitems/backgrounds/%s.jpg" % (settings.MEDIA_ROOT, mongoid))
-        img.write(jpegwritepath)
-        remove_tmp_file = os.remove("%sitems/backgrounds/%s" % (settings.MEDIA_ROOT, mongoid))
-
-    #---------------------------------------------------------------------------
-    def deserialize(self, method, endpoint, body):
-        format = None
-        """
-        Changes request stat in to python objects
-        """
-        if not format:
-            format = self.request.META.get('CONTENT_TYPE', 'application/json')
-
-        if format == 'application/x-www-form-urlencoded':
-            return self.request.POST
-
-        if format.startswith('multipart'):
-            multipart_data = self.request.POST.copy()
-            multipart_data.update(self.request.FILES)
-            return multipart_data
-
-        return super(ItemHandlingResource, self).deserialize(method, endpoint, format)
-
-    #---------------------------------------------------------------------------
     @classmethod
     def urls(cls, name_prefix=None):
         urlpatterns = super(ItemHandlingResource, cls).urls(name_prefix=name_prefix)
@@ -143,4 +92,4 @@ class ItemHandlingResource(DjangoResource):
             url(r'^item/pdf$', csrf_exempt(cls.as_view('fileupload')), name=cls.build_url_name('fileupload', name_prefix)),
             url(r'^item/cover$', csrf_exempt(cls.as_view('coverupload')), name=cls.build_url_name('coverupload', name_prefix)),
             url(r'^item/background$', csrf_exempt(cls.as_view('backgroundupload')), name=cls.build_url_name('backgroundupload', name_prefix))
-        )
+            )
